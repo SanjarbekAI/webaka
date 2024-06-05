47ca34033516499b5b9499bc7b7d095c3ba24da9
SanjarbekAI <sanjarbekwork@gmail.com>
2024-06-05 13:34:41 +0500

    handle request

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from webob import Request, Response
 class WebAka:
     def __call__(self, environ, start_response):
         request = Request(environ)
-        response = Response()
-        response.text = "Hello world"
+        response = self.handle_request(request)
         return response(environ, start_response)
+
+    def handle_request(self, request):
+        user_agent = request.environ.get('HTTP_USER_AGENT', 'USER_AGENT_NOT_FOUND')
+        response = Response()
+        response.text = f"Hello friend, {user_agent}"
+        return response
