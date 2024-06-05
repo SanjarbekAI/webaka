f1daccd73f2ad27d9fb3c5d3fe6948678984ed9d
SanjarbekAI <sanjarbekwork@gmail.com>
2024-06-05 13:31:36 +0500

    webob is added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from webob import Request, Response
+
+
 class WebAka:
     def __call__(self, environ, start_response):
-        status = "200 OK"
-        response_headers = [('Content-type', 'text/plain')]
-        start_response(status, response_headers)
-        return [b"Hello World!"]
+        request = Request(environ)
+        response = Response()
+        response.text = "Hello world"
+        return response(environ, start_response)
