f00c811710859475d25eb7dbed40d991182f903b
SanjarbekAI <sanjarbekwork@gmail.com>
2024-06-05 13:25:50 +0500

    code changed to class

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def app(environ, start_response):
-    status = "200 OK"
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    return [b"Hello World!"]
+from app import WebAka
+
+app = WebAka()
