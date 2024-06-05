128449043010feb6725020d58494790919b44990
SanjarbekAI <sanjarbekwork@gmail.com>
2024-06-05 13:22:51 +0500

    gunicorn is added

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from wsgiref.simple_server import make_server
-
-
-def simple_app(environ, start_response):
+def app(environ, start_response):
     status = "200 OK"
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return [b"Hello World!"]
-
-
-server = make_server('localhost', 8000, simple_app)
-server.serve_forever()
