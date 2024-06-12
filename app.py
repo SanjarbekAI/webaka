0974e604e912c1337fd6bfa719015fbe38959852
SanjarbekAI <sanjarbekwork@gmail.com>
2024-06-12 08:47:36 +0500

    routing is added

--- app.py
+++ app.py
@@ -2,13 +2,34 @@ from webob import Request, Response
 
 
 class WebAka:
+    def __init__(self):
+        self.routes = dict()
+
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
 
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        return wrapper
+
     def handle_request(self, request):
-        user_agent = request.environ.get('HTTP_USER_AGENT', 'USER_AGENT_NOT_FOUND')
         response = Response()
-        response.text = f"Hello friend, {user_agent}"
+        handler = self.find_handler(request)
+        if handler is not None:
+            handler(request, response)
+        else:
+            self.default_response(response)
+
         return response
+
+    def find_handler(self, request):
+        if request.path in self.routes:
+            return self.routes[request.path]
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not Found"
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from app import WebAka
 
 app = WebAka()
+
+
+@app.route('/home')
+def home(request, response):
+    response.text = "Hello from the Home Page"
+
+
+@app.route('/about')
+def about(request, response):
+    response.text = "Hello from the About Page"
